85a066815342cbf8bd52433e1645a1c404401517
GaiaTuven <ggwiese@gmail.com>
2025-06-20 07:00:59 +0200

    Operators

---
+++ Python/operators.py
@@ -0,0 +1,26 @@
+# Operators
+
+# Plus operator
+# minus operator
+# multiply operator
+# divide operator
+# Exponent operator
+
+x = 10
+y = 5
+
+# Plus operator
+print(f"Plus: {x + y}")  # Output: 15
+# Minus operator
+print(f"Minus: {x - y}")  # Output: 5
+# Multiply operator
+print(f"Multiply: {x * y}")  # Output: 50
+# Divide operator
+print(f"Divide: {x / y}")  # Output: 2.0
+# Exponent operator
+print(f"Exponent: {x ** y}")  # Output: 100000
+# Modulus operator
+print(f"Modulus: {x % y}")  # Output: 0
+# Floor division operator
+print(f"Floor Division: {x // y}")  # Output: 2
+
