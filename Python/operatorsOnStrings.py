4dc2468d39986506d06a84e007f7a5c34ffbb7d7
GaiaTuven <ggwiese@gmail.com>
2025-06-20 07:04:40 +0200

    operators on strings

---
+++ Python/operatorsOnStrings.py
@@ -0,0 +1,12 @@
+# Operators with strings
+
+string1 = "Hello"
+string2 = "World"
+
+# Concatenation operator
+print(f"Concatenation: {string1 + ' ' + string2}")  # Output: Hello World
+# Repetition operator
+print(f"Repetition: {string1 * 3}")  # Output: HelloHello
+# Length operator
+print(f"Length of string1: {len(string1)}")  # Output: 5
+print(f"Length of string2: {len(string2)}")  # Output: 5
