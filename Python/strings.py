c968753bc8e835c8da6be0e324db2adc6a20690e
GaiaTuven <ggwiese@gmail.com>
2025-06-20 06:46:40 +0200

    Python folder

---
+++ Python/strings.py
@@ -0,0 +1,21 @@
+# Strings
+
+message = "Hello, World!"
+
+print(message)
+
+
+# Advanced concepts
+
+message = "Hello, World!"
+print(message[0])
+print(message[1])
+
+# String length
+print(len(message))
+
+print(message.strip())  # Remove whitespace
+print(message.lower())  # Convert to lowercase
+print(message.upper())  # Convert to uppercase
+print(message.replace("World", "Python"))  # Replace substring
+print(message.split(","))  # Split string into a list
