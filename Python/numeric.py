d4201198537ad7470d0322d3596fcc881df742b8
GaiaTuven <ggwiese@gmail.com>
2025-06-20 06:50:25 +0200

    numberic formats

---
+++ Python/numeric.py
@@ -0,0 +1,9 @@
+# Numeric data
+
+number = 3
+print(f"Number: {number}")
+print(type(number))  # Check type of number
+
+number2 = 3.14
+print(f"Number2: {number2}")
+print(type(number2))  # Check type of number2
